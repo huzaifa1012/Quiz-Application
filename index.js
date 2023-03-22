4f36295a50b5465b3a67ab2fb8473353a79c5e2c
huzaifahux <www.pakteach@gmail.com>
2023-03-23 00:38:20 +0500

    JS working start

---
+++ index.js
@@ -0,0 +1,34 @@
+let data = [
+    {
+        q: 'What kind of language is HTML ?',
+        opt1: "Programming",
+        opt2: "Styling",
+        opt3: "Markup",
+        opt4: "Testing",
+        a: "Markup"
+    },
+    {
+        q: 'Do we face Tags in HTML ?',
+        opt1: "No",
+        opt2: "Yes",
+        opt3: "Sometimes",
+        opt4: "Not Now",
+        a: "Yes"
+    },
+    {
+        q: 'When did HTML 5 come out  ?',
+        opt1: "1993",
+        opt2: "2003",
+        opt3: "2019",
+        opt4: "2014",
+        a: "2014"
+    }
+]
+
+
+let index = 0;
+const auto = () => {
+    let question = document.getElementById('question')
+    question.innerText = data[index].q
+}
+auto()
