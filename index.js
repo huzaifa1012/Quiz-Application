2b597690e09d45b006102a47417949f0bf404ab3
huzaifahux <www.pakteach@gmail.com>
2023-03-23 16:05:39 +0500

    checking answer correct or not

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let data = [
         opt2: "Styling",
         opt3: "Markup",
         opt4: "Testing",
-        a: "Markup"
+        a: "opt3"
     },
     {
         q: 'Do we face Tags in HTML ?',
@@ -13,7 +13,7 @@ let data = [
         opt2: "Yes",
         opt3: "Sometimes",
         opt4: "Not Now",
-        a: "Yes"
+        a: "opt2"
     },
     {
         q: 'When did HTML 5 come out  ?',
@@ -21,26 +21,68 @@ let data = [
         opt2: "2003",
         opt3: "2019",
         opt4: "2014",
-        a: "2014"
+        a: "opt4"
     }
 ]
 
 
 let index = 0;
-const auto = () => {
-    let question = document.getElementById('question')
-    question.innerText = data[index].q
-    let options = document.querySelectorAll('.options')
+const question = document.getElementById('question')
+const options = document.querySelectorAll('.options')
+const total = options.length
+let right = 0
+let wrong = 0
 
+const auto = () => {
+    question.innerText = data[index].q
     options[0].nextElementSibling.innerText = data[index].opt1
     options[1].nextElementSibling.innerText = data[index].opt2
     options[2].nextElementSibling.innerText = data[index].opt3
     options[3].nextElementSibling.innerText = data[index].opt4
+}
+
+const submitQuiz = () => {
+    let answers = next()
+    if (answers === data[index].a) {
+        right++
+        console.log("right", right)
+    }
+    else {
+        wrong++
+        console.log("worng", wrong)
+    }
+    index++
+    auto()
 
 }
-auto()
+
+
+// const next = () => {
+//     let selectedOption;
+//     options.forEach((input) => {
+//       if (input.checked) {
+//         selectedOption = input.value;
+//       }
+//     });
+//     return selectedOption || 'No answer selected';
+//   }
 
 
 const next = () => {
-    console.log(radio1.checked, radio2.checked, radio3.checked, radio4.checked);
+    let selectedOption
+    options.forEach(
+        (input) => {
+            if (input.checked) {
+                selectedOption = input.value
+            }
+
+        })
+    return selectedOption
 }
+
+
+
+
+
+
+auto()
